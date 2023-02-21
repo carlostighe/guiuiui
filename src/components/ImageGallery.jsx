605172ca46343540d7ee0eb3b0d46753520c324c
guiuiuiuiuiui <125576025+guiuiuiuiuiui@users.noreply.github.com>
2023-02-21 14:53:53 -0500

    Update ImageGallery.jsx

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -4,17 +4,32 @@ function ImageGallery({ folderPath }) {
   const [imagePaths, setImagePaths] = useState([]);
 
   useEffect(() => {
-    fetch(`/images?folderPath=${folderPath}`)
-      .then(response => response.json())
-      .then(data => setImagePaths(data));
+    fetch(`/images/${folderPath}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        setImagePaths(data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
   }, [folderPath]);
 
   return (
+    // empty 
+    console.log(imagePaths)
+    /*
     <div>
       {imagePaths.map((imagePath, index) => (
         <img key={index} src={imagePath} alt={`Image ${index}`} />
       ))}
     </div>
+    */
   );
 }
 
