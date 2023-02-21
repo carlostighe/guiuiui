5fd541139e82d016aefd3817504ffb3c854c61ec
Carlos <carlostighe@gmail.com>
2023-02-21 20:17:17 +0000

    Update flask.py
    add code to run your flask app

--- flask.py
+++ flask.py
@@ -16,3 +16,7 @@ def images(folder_name):
             url = f'/images/{folder_name}/{file_name}'
             images.append(url)
     return jsonify(images)
+
+# Need to run the flask app
+if __name__ == '__main__':
+ app.run()
